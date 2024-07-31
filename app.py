8a62a519d569360e4351415f82394a1939912525
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-07-31 11:31:06 +0200

    flask

--- app.py
+++ app.py
@@ -2,8 +2,13 @@ from flask import Flask
 
 app = Flask(__main__)
 
+@app.route("/")
 def home():
-    return "hello this is the main page<HELLO"
+    return "hello this is the main page <HELLO<h1>"
+
+@app.route("/<name>")
+def user(name):
+    retu
 
 if __name__ == "__main__":
    app.run()
